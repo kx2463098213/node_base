f0bf4a7edd1965f23c907761814c255b4b24e654
xiaozhonggao <xiaozhonggao@sunteng.com>
2026-02-24 18:38:29 +0800

    opt: 优化 app.module.ts 的 import 路径

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
 import {ConfigModule} from "@nestjs/config";
-import { DeployModule } from "./modules/deploy/deploy.module";
-import { DatabaseModule } from "./core/database/database.module";
-import { GlobalModule } from "./global.module";
+import { DeployModule } from "@/modules/deploy/deploy.module";
+import { DatabaseModule } from "@/core/database/database.module";
+import { GlobalModule } from "@/global.module";
 import { config } from "./core/config";
 import { APP_GUARD, APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from "@nestjs/core";
-import { UCAuthGuard } from "./core/guards/auth.guard";
+import { UCAuthGuard } from "@/core/guards/auth.guard";
 import { LabelModule } from "./modules/label/label.module";
-import { ScopeStoreMiddleware } from "./core/middleware/scope-store.middleware";
-import { RequestLogMiddleware } from "./core/middleware/req-log-middleware";
-import { CustomExceptionFilter } from "./core/filters/custom-exception.filter";
-import { TransformInterceptor } from "./core/interceptors/transform.interceptor";
+import { ScopeStoreMiddleware } from "@/core/middleware/scope-store.middleware";
+import { RequestLogMiddleware } from "@/core/middleware/req-log-middleware";
+import { CustomExceptionFilter } from "@/core/filters/custom-exception.filter";
+import { TransformInterceptor } from "@/core/interceptors/transform.interceptor";
 import { I18nValidationPipe } from 'nestjs-i18n';
 
 @Module({
